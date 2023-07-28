be6efa1e0ccbcdf71f606c11f4000eaffdc03478
bonface murangiri <bonnyrangi95@gmail.com>
2023-07-28 19:20:15 +0000

    simple shell logics

--- shell_loop.c
+++ shell_loop.c
@@ -13,7 +13,7 @@ void shell_prompt(shell_struct *arg)
 
 	while (prompt)
 	{
-		write(STDOUT_FILENO, "$ ", 2);
+		write(STDIN_FILENO, "$ ", 3);
 		cmd_input = _readline(&in_eof);
 
 		if (in_eof != -1)
